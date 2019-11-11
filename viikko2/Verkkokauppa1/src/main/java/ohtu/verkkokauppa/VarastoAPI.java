b4bc3720dbfc806b95714532e3737b0848daa5ed
kriskrok <25787775+kriskrok@users.noreply.github.com>
2019-11-11 20:00:30 +0200

    Adds 2.9 riippuvuuksien injektointi osa 3: Verkkokauppa

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/VarastoAPI.java
@@ -0,0 +1,11 @@
+package ohtu.verkkokauppa;
+
+public interface VarastoAPI {
+    Tuote haeTuote(int id);
+
+    int saldo(int id);
+
+    void otaVarastosta(Tuote t);
+
+    void palautaVarastoon(Tuote t);
+}
