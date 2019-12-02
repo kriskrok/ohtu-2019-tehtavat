33a6f7cce9dabe23195300915267e82f10c37485
kriskrok <25787775+kriskrok@users.noreply.github.com>
2019-12-02 21:21:46 +0200

    Adds 5.4 Laskin ja komento-oliot

---
+++ viikko5/LaskinFX8/src/main/java/laskin/Erotus.java
@@ -0,0 +1,22 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Erotus extends Komento {
+
+    public Erotus(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        sovellus.miinus(lueLuku());
+        paivitaNaytto();
+    }
+
+    @Override
+    public void peru() {
+
+    }
+}
---
+++ viikko5/LaskinFX8/src/main/java/laskin/Summa.java
@@ -0,0 +1,22 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Summa extends Komento {
+
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        sovellus.plus(lueLuku());
+        paivitaNaytto();
+    }
+
+    @Override
+    public void peru() {
+        
+    }
+}
